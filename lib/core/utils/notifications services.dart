5ac6400724d5feff338d811bb249203535e39d26
Mahmoud swilam <123952365+swilam202@users.noreply.github.com>
2023-09-18 23:49:27 +0300

    refactor notifications class

--- lib/core/utils/notifications services.dart
+++ lib/core/utils/notifications services.dart
@@ -2,21 +2,25 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class NotificationsServices{
-  static sendNotification()async{
-    String url = 'https://fcm.googleapis.com/fcm/send';
-    var response = await http.post(
+  static const String url = 'https://fcm.googleapis.com/fcm/send';
+  static const String serverKey = 'AAAAKYJSVVU:APA91bFlK2UFZDBxeBR7D36xP50CiumK7SOUQ4_qv9vBRHIoubN1iobbXinthCOhVgm8RSREl4Q32elxBgE9PHceSeFvVabdKafoqghbbswuliWDd8w0ObDl7pWHQwCHRH33mWKnmAxA';
+  static const Map<String,String> headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'key=',
+    };
+
+  static pushNotification({required String token,required String title,required String body})async{
+  //cJjSOFAMRRC49aNoxhljp4:APA91bG8vyY-TFN1PNWmwSQSFCWKHmcRGQo5h5gzyT248hL0Or5545h8vYTqz27dXVxLkHfN6DhthVr1NIoiVQk13dBUQdEaIb4nuXfa_IjE716KvvuEN23yhaPQXfufGDxc-t8KBQ3q  
+    await http.post(
       Uri.parse(url),
       body: jsonEncode({
-    "to":"cJjSOFAMRRC49aNoxhljp4:APA91bG8vyY-TFN1PNWmwSQSFCWKHmcRGQo5h5gzyT248hL0Or5545h8vYTqz27dXVxLkHfN6DhthVr1NIoiVQk13dBUQdEaIb4nuXfa_IjE716KvvuEN23yhaPQXfufGDxc-t8KBQ3q",
+    "to": token,
     "notification":{
-        "title":"title",
-        "body":"body"
-    }
-}),
-headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'key=AAAAKYJSVVU:APA91bFlK2UFZDBxeBR7D36xP50CiumK7SOUQ4_qv9vBRHIoubN1iobbXinthCOhVgm8RSREl4Q32elxBgE9PHceSeFvVabdKafoqghbbswuliWDd8w0ObDl7pWHQwCHRH33mWKnmAxA',
-    },
+        "title":title,
+        "body":body,
+          }
+        }),
+    headers: headers,
 
     ).then((value) => print('doneeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'));
   }
--- lib/features/home screen/presentation/views/home page.dart
+++ lib/features/home screen/presentation/views/home page.dart
@@ -60,23 +60,22 @@ class _HomePageState extends State<HomePage> {
             
     print('+++++++++++++++++++++++++++++++++++++++++++++++++');
     print(await messaging.getToken());
-  
     print('+++++++++++++++++++++++++++++++++++++++++++++++++');
           }, icon: Icon(Icons.add),),
-          IconButton(onPressed: ()async{
+         /* IconButton(onPressed: ()async{
             NotificationsServices.sendNotification();
             setState(() {
               
             });
-          /*  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+            FirebaseMessaging.onMessage.listen((RemoteMessage message) {
   print('Got a message whilst in the foreground!');
   print('Message data: ${message.data}');
 
   if (message.notification != null) {
     print('Message also contained a notification: ${message.notification}');
   }
-});*/
-          }, icon: Icon(Icons.search),),
+});
+          }, icon: Icon(Icons.search),),*/
         ],
       ),
       body: HomeBody(),
